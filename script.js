dd6be79196e4627218b7ff65a230987627422ef9
mjiang95 <minniejiang2013@gmail.com>
2021-11-12 13:59:22 -0800

    some changes

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const poems = [
   },
   {
     author: "Minnie",
-    poem: "Roses are red",
-    image: "random.png",
+    poem: "Roses are red, violets are blue. My life is a mess what should I do?",
+    image: "rose.jpeg",
   },
 
 ];
